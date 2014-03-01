3eed500d2a4f444168b80d50fd7fee3e71eb55c9
williamzh <williamzh87@gmail.com>
2014-03-01 13:00:29 +0100

    Added basic templates

---
+++ RecipeRepo/MainApp/Controllers/RecipeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainApp.Controllers
+{
+    public class RecipeController : Controller
+    {
+        //
+        // GET: /Recipe/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
