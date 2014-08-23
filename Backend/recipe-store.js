2d6eb1e8b23d7f114909e4458398057994ee5808
williamzh <williamzh87@gmail.com>
2014-08-23 10:42:12 +0200

    More unit tests

--- Backend/recipe-store.js
+++ Backend/recipe-store.js
@@ -4,45 +4,56 @@ exports = module.exports = (function recipeController() {
 
 	function addRecipe(recipe, successCallback, errorCallback) {
 		if(!recipe) {
-			errorCallback('Recipe must be supplied');
+			errorCallback('Recipe must be supplied.');
+			return;
 		}
 
 		if(!recipe.id) {
-			errorCallback('Recipe ID must be supplied');
+			errorCallback('Recipe ID must be supplied.');
+			return;
 		}
 
-		getRecipe(recipe.id, function(data) {
-			if(data.indexOf(recipe) > -1) {
-				errorCallback('Recipe with ID ' + recipe.id + ' already exists.');
-				return;
+		var config = {
+			url: 'http://localhost:9200/reciperepo/recipe/' + recipe.id,
+			method: 'GET'
+		};
+
+		request(config, function(error, response, data) {
+			if(response.statusCode == 404) {
+				var config = {
+					url: 'http://localhost:9200/reciperepo/recipe/' + recipe.id,
+					method: 'PUT',
+					body: JSON.stringify(recipe)
+				};
+
+				request(config, function(error, response, data) {
+					if(!error && response.statusCode == 201) {
+						successCallback('Recipe successfully added.');
+					}
+					else {
+						errorCallback(error);
+					}
+				});
 			}
-
-			var config = {
-				url: 'http://localhost:9200/reciperepo/recipe/',
-				method: 'PUT',
-				body: recipe
-			};
-
-			request(config, function(error, response, data) {
-				if(!error && response.statusCode == 200) {
-					successCallback(data);
-				}
-				else {
-					errorCallback(error);
-				}
-			});
-		}, errorCallback);
+			else if(response.statusCode == 200) {
+				errorCallback('Recipe with ID ' + recipe.id + ' already exists.');
+			}
+			else {
+				errorCallback('Failed to add recipe. See error log for details.');
+			}
+		});
 	}
 
 	function getAllRecipes(successCallback, errorCallback) {
 		var config = {
-			url: 'http://localhost:9200/reciperepo/_search/',
+			url: 'http://localhost:9200/reciperepo/_search',
 			method: 'GET'
 		};
 
 		request(config, function(error, response, data) {
 			if(!error && response.statusCode == 200) {
-				successCallback(data);
+				var parsedData = JSON.parse(data);
+				successCallback(parsedData.hits.hits);
 			}
 			else {
 				errorCallback(error);
@@ -57,8 +68,12 @@ exports = module.exports = (function recipeController() {
 		};
 
 		request(config, function(error, response, data) {
-			if(!error && response.statusCode == 200) {
-				successCallback(data);
+			if(response.statusCode == 200) {
+				var parsedData = JSON.parse(data);
+				successCallback(parsedData._source);
+			}
+			else if(response.statusCode == 404) {
+				successCallback(null);
 			}
 			else {
 				errorCallback(error);
--- Backend/test/recipe-store-spec.js
+++ Backend/test/recipe-store-spec.js
@@ -4,57 +4,156 @@ var recipeStore = require('../recipe-store');
 
 describe('Provided a RecipeStore', function() {
 	
+	var elasticSearchPath = 'http://localhost:9200';
+
 	describe('when adding a recipe', function() {
 		it('should return error if recipe is not defined', function(done) {
-			recipeStore(undefined, undefined, function (error) {
-				assert.equal(error, 'Recipe must be supplied');
+			recipeStore.add(undefined, undefined, function (error) {
+				assert.equal(error, 'Recipe must be supplied.');
 				done();
 			});
 		});
 
 		it('should return error if recipe ID is not defined', function(done) {
-			recipeStore({ id: undefined }, undefined, function (error) {
-				assert.equal(error, 'Recipe ID must be supplied');
+			recipeStore.add({ id: undefined }, undefined, function (error) {
+				assert.equal(error, 'Recipe ID must be supplied.');
 				done();
 			});
 		});
 
 		it('should return error if recipe already exists', function(done) {
-			nock('http://localhost:9200')
+			nock(elasticSearchPath)
 				.get('/reciperepo/recipe/1')
-				.reply(200, {[
-					id: 1,
-					name: 'Spaghetti Bolognese'
-				]});
+				.reply(200, {
+				   "_index": "reciperepo",
+				   "_type": "recipe",
+				   "_id": "1",
+				   "_version": 12,
+				   "found": true,
+				   "_source": {
+				      "id": 1
+				   }
+				});
 
-			recipeStore({ id: 1 }, undefined, function(error) {
+			recipeStore.add({ id: 1 }, undefined, function(error) {
 				assert.equal(error, 'Recipe with ID 1 already exists.');
 				done();
 			});
 		});
 
 		it('should add the recipe otherwise', function(done) {
-			var expectation = nock('http://localhost:9200')
-				.put('reciperepo/recipe/1')
-				.reply(200, {
-					// TODO: add ES response
+			nock(elasticSearchPath)
+				.get('/reciperepo/recipe/1')
+				.reply(404, {
+				   "_index": "reciperepo",
+				   "_type": "recipe",
+				   "_id": "2",
+				   "found": false
 				});
 
-			recipeStore({ id: 1 }, function(result) {
-				assert.equal(expectation.isDone(), true);
+			nock(elasticSearchPath)
+				.put('/reciperepo/recipe/1')
+				.reply(201);
+
+			recipeStore.add({ id: 1 }, function(result) {
+				assert.equal(result, 'Recipe successfully added.');
 				done();
 			});
 		});
 	});
 
 	describe('when getting all recipes', function() {
-		it('should return all existing recipes', function() {
-			
+		it('should return all existing recipes', function(done) {
+			nock(elasticSearchPath)
+				.get('/reciperepo/_search')
+				.reply(200, {
+					"hits": {
+				    	"total": 1,
+				    	"max_score": 1,
+				    	"hits": [{
+				            "_index": "reciperepo",
+				            "_type": "recipe",
+				            "_id": "1",
+				            "_score": 1,
+				            "_source": {
+				               "id": 1
+				            }
+				        }, {
+				            "_index": "reciperepo",
+				            "_type": "recipe",
+				            "_id": "2",
+				            "_score": 1,
+				            "_source": {
+				               "id": 2
+				            }
+				        }]
+					}
+				});
+
+			recipeStore.getAll(function(hits) {
+				assert.equal(hits.length, 2);
+				done();
+			});
 		});
 
-		it('should return null if there are no recipes', function() {
+		it('should return empty array if there are no recipes', function(done) {
+			nock(elasticSearchPath)
+				.get('/reciperepo/_search')
+				.reply(200, {
+				   "hits": {
+				    	"total": 0,
+				    	"max_score": null,
+				    	"hits": []
+					}
+				});
 
+			recipeStore.getAll(function(hits) {
+				assert.equal(hits.length, 0);
+				done();
+			});
 		});
+	});
+
+	describe('when getting recipe by ID', function() {
+		it('should return recipe if it exists', function(done) {
+			nock(elasticSearchPath)
+				.get('/reciperepo/recipe/1')
+				.reply(200, {
+				   "_index": "reciperepo",
+				   "_type": "recipe",
+				   "_id": "1",
+				   "_version": 12,
+				   "found": true,
+				   "_source": {
+				      "id": 1
+				   }
+				});
+
+			recipeStore.get('1', function(recipe) {
+				assert.equal(recipe.id, '1');
+				done();
+			});
+		});
+
+		it("should return null if recipe doesn't exist", function(done) {
+			nock(elasticSearchPath)
+				.get('/reciperepo/recipe/1')
+				.reply(404, {
+					"_index": "reciperepo",
+				    "_type": "recipe",
+				    "_id": "1",
+				    "found": false
+				});
+
+			recipeStore.get('1', function(recipe) {
+				assert.equal(recipe, null);
+				done();
+			});
+		});
+	});
+
+	describe('when updating an existing recipe', function() {
+		
 	});
 	
 });
